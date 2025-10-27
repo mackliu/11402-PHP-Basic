5a2637bdc726f7164c7d3a6c7c1fffa0d529af79
mackliu <macklun@gmail.com>
2025-10-27 14:44:12 +0800

    菱形

--- pra05.php
+++ pra05.php
@@ -49,11 +49,60 @@ for($i=0;$i<5;$i++){
 *********<br>
 
 
+<h2>矩形-挖空</h2>
+<?php
+for($i=0;$i<7;$i++){
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6 || $j==0 || $j==6){
+
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+
+?>
+
+<h2>菱形</h2>
+<?php 
+
+$x=10;
+$y=0;
+
+if($x%2==0){
+    $x=$x+1;
+}
+
+for($i=0;$i<$x;$i++){
+    if($i>floor($x/2)){
+        $y=$y-1;
+    }else{
+        $y=$i;
+    }
+    //echo $i."-".$y;
+
+
+    for($k=0;$k<floor($x/2)-$y;$k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0;$j<2*$y+1;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
 
 
 
+?>
+
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
+
+
+
