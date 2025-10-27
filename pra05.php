dc8f7950f174ba8a6e76f36b70d5b8e76248d2b8
mackliu <macklun@gmail.com>
2025-10-27 13:45:50 +0800

    正三角形

--- pra05.php
+++ pra05.php
@@ -1,3 +1,9 @@
+<style>
+    * {
+        font-family:'Courier New', Courier, monospace
+    }
+</style>
+
 <h2>直角三角形</h2>
 <?php
 for($i=0;$i<5;$i++){
@@ -16,5 +22,38 @@ for($i=0;$i<5;$i++){
     }
     echo "<br>";
 }
+?>
+<h2>正三角型</h2>
+<?php
+
+for($i=0;$i<5;$i++){
+     
+    for($k=0;$k<4-$i;$k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0;$j<2*$i+1;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
 
 
+?>
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;*<br>
+&nbsp;&nbsp;&nbsp;***<br>
+&nbsp;&nbsp;*****<br>
+&nbsp;*******<br>
+*********<br>
+
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
