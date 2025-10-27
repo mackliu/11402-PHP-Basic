afb205b245c7af13d446460100561f1b5ba54d91
mackliu <macklun@gmail.com>
2025-10-27 16:19:11 +0800

    挖空及對角線

--- pra05.php
+++ pra05.php
@@ -98,6 +98,63 @@ for($i=0;$i<$x;$i++){
 
 ?>
 
+
+<h2>矩形-對角線</h2>
+<?php
+$x=13;
+
+for($i=0;$i<$x;$i++){
+    for($j=0;$j<$x;$j++){
+        if($i==0 || $i==$x-1 || $j==0 || $j==$x-1 || $i==$j || ($j+$i)==$x-1){
+
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+
+?>
+<h2>菱形-對角線</h2>
+<?php 
+
+$x=10;
+$y=0;
+
+if($x%2==0){
+    $x=$x+1;
+}
+$mid=floor($x/2);
+
+for($i=0;$i<$x;$i++){
+    if($i>$mid){
+        $y=$y-1;
+    }else{
+        $y=$i;
+    }
+
+    //echo $i."-".$y;
+
+
+    for($k=0;$k<$mid-$y;$k++){
+        echo "&nbsp;";
+    }
+    //echo $y;
+    for($j=0;$j<2*$y+1;$j++){
+        
+        if($j==0 || $j==2*$y || $j==$y || $i==$mid){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+
+
+
+?>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
