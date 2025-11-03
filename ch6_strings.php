baf6ed27c412a6d344a71806b64bed00068669e2
mackliu <macklun@gmail.com>
2025-11-03 09:46:17 +0800

    字串函式使用練習

--- ch6_strings.php
+++ ch6_strings.php
@@ -35,3 +35,54 @@ echo "<br>";
 echo "取代後:".$newstr;
 
 ?>
+
+<h2>字串分割</h2>
+
+<li>將”this,is,a,book”依”,”切割後成為陣列</li>
+<?php
+$str='this,is,a,book';
+$newstr=explode(",",$str);
+echo "<pre>";
+print_r($newstr);
+echo "</pre>";
+?>
+
+<h2>字串組合</h2>
+
+</li>將上例陣列重新組合成"this is a book"</li>
+<?php
+
+$combine=join(" ",$newstr);
+echo "<hr>";
+echo $combine;
+
+?>
+
+
+<h2>子字串取用</h2>
+
+<li>將"The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…"</li>
+<?php
+$str="The reason why a great man is great is that he resolves to be a great man";
+$pre=mb_substr($str,0,10);
+
+echo $pre;
+echo "...";
+
+?>
+
+
+
+<h2>尋找字串與HTML、css整合應用</h2>
+
+<ul>
+    <li>給定一個句子，將指定的關鍵字放大</li>
+    <li>“學會PHP網頁程式設計，薪水會加倍，工作會好找”</li>
+    <li>請將上句中的 “程式設計” 放大字型或變色.</li>
+</ul>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
